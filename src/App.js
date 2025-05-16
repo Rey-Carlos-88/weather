76e6fdcfe40d402d97b5258d02f5a55f1209dc10
TheKing88 <canogsdc@gmail.com>
2025-05-16 02:16:04 -0600

    add and update files and settings to using tailwind css

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
+import WeatherDashboard  from './components';
 import './index.css';
 
 function App() {
   return (
-    <div className="bg-blue-800 text-gray-500 p-4">
+    <>
+     <div className="bg-blue-800 text-gray-500 p-4">
       ¡Tailwind debería funcionar ahora!
     </div>
+    <WeatherDashboard  />
+    </>
   );
 }
 
 export default App;
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,15 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html"
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {},
   },
   plugins: [],
